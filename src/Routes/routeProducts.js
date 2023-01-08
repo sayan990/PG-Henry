70f6f8caa5d7ce1c8048a864f7e4d16f8f8a7919
ale2trelew <ale2trelew@gmail.com>
2023-01-08 09:31:39 -0300

    tuve que reinstalar nodemon por alguna razon

--- src/Routes/routeProducts.js
+++ src/Routes/routeProducts.js
@@ -23,8 +23,6 @@ router.get('/zapatillas', async (req, res) => {
             const zapatillas = await zapSchema.find();
             const zapasFiltradas = zapatillas.filter(obj => obj.modelo.toLowerCase().includes(modelo.toLowerCase()));
 
-            console.log("ESTO TIENEN LAS ZAPATILLAS: ", zapasFiltradas);
-
             if (zapasFiltradas?.length) return res.status(200).send(zapasFiltradas);
             else return res.status(404).send(`El modelo "${modelo}" no existe.`);
         }
