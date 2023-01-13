6bcc6cf293a557df0991a5085c53600035de95af
sayan990 <maximilianocaceres1601@gmail.com>
2023-01-13 17:14:13 -0300

    modified:   package.json 	modified:   src/index.js 	new file:   src/uploadImage.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const routeProducts = require('./Routes/routeProducts.js')
 const routeUsers = require ('./Routes/routeUsers.js')
 const routeFilters = require('./Routes/routeFilters.js')
+const uploadImage = require("./uploadImage.js")
 
 
 const app = express()
@@ -17,6 +18,18 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
+
+app.post("/uploadImage", (req, res) =>{
+    uploadImage(req.body.image)
+    .then((url) => res.send(url))
+    .catch((err) => res.status(500).send(err))
+})
+
+app.post("/uploadMultipleImages", (req, res) => {
+    uploadImage.uploadMultipleImages(req.body.images)
+      .then((urls) => res.send(urls))
+      .catch((err) => res.status(500).send(err));
+  });
 
 app.use('/products', routeProducts, routeUsers)
 app.use('/products/filtros', routeFilters)
