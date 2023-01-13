114be9a00b23ba1c32ca4278e97bdb30ccfc844e
ale2trelew <ale2trelew@gmail.com>
2023-01-13 18:46:16 -0300

    back de mercadopago funcionando, falta modularizarlo

--- src/mercado pago/mercadopago.js
+++
@@ -1,9 +0,0 @@
-const mercadopago = require("mercadopago");
-require("dotenv").config();
-//crea objeto de preferencia
-
-mercadopago.configure({
-    access_token: process.env.ACCESS_TOKEN,
-});
-
-module.exports = { mercadopago }
