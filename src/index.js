f1a21cb3f596c407df0b9d8e48b6e38e90472dd7
sayan990 <maximilianocaceres1601@gmail.com>
2023-01-23 16:53:31 -0300

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,18 @@ const routeProducts = require('./Routes/routeProducts.js')
 const routeUsers = require ('./Routes/routeUsers.js')
 const routeFilters = require('./Routes/routeFilters.js')
 const uploadImage = require("./uploadImage.js")
+const {Server} = require("socket.io")
+
 
 const routeOrders = require ('./Routes/orderRoutes.js')
 
 const app = express()
 const port = process.env.PORT || 3001
+const io = new Server({
+    cors:{
+        origin:"http://localhost:3000"
+    }
+})
 
 app.use(express.json());
 
@@ -37,9 +44,17 @@ app.use('/productos/filtros', routeFilters);
 app.use('/usuarios', routeUsers);
 app.use('/pedido', routeOrders);
 
+io.on("connection", (socket) =>{
+    socket.on('notificacion', msg => {
+        io.emit('notificacion', msg);
+      });
+})
+
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('conectado a mongo'))
     .catch((e) => console.log(e))
 
+
+io.listen(5000)
 app.listen(port, console.log(`listening port ${port}`))
